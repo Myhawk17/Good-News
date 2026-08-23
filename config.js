d400fd045dbdd722b26ea322bd8bf411fe9902e4
Myhawk17 <myhawk17@gmail.com>
2026-08-23 11:44:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // Trage hier NUR Project URL + Publishable Key (oder anon key) ein.
 // Niemals einen service_role / secret key in diese Datei eintragen.
 window.APP_CONFIG = {
-  SUPABASE_URL: "DEINE_SUPABASE_URL",
-  SUPABASE_ANON_KEY: "DEIN_PUBLISHABLE_ODER_ANON_KEY",
-  APP_NAME: "Daily Slides"
+  SUPABASE_URL: "https://hkhuspzqhagodxtujxkv.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhraHVzcHpxaGFnb2R4dHVqeGt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc0NjU4MjQsImV4cCI6MjEwMzA0MTgyNH0.WPVUi13m6igkNHC4EbJZIV9G5lB51BfYfbvKtCuyzzI",
+  APP_NAME: "Good News"
 };
