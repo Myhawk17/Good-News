1e8de5c088079b3ee912faef054a8a1174b3db0f
Myhawk17 <myhawk17@gmail.com>
2026-08-28 08:14:38 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ if(launchSplash){
   launchSplash.addEventListener("animationend",(e)=>{
     if(e.animationName==="launchSplashOut") removeLaunchSplash();
   });
-    setTimeout(removeLaunchSplash,4700);
+      setTimeout(removeLaunchSplash,3200);
 }
 
 const cfg = window.APP_CONFIG || {};
